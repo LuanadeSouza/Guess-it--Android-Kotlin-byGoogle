238bbf4f1341bcf9100a66bb70b0ae2ff5926639
Luana Souza <luana.souza@ifood.com.br>
2020-11-16 08:57:19 -0300

    Add a GameViewModel data binding to GameFragment layout, Add view model variable and data binding to handle clicling, GameViewModel into the data binding, Add a ScoreViewModel data binding variable to ScoreFragment, Use the view model variable and data binding to handle4 cliclink, Into the data binding and removbe OnClickListener setup for payAgainButton

--- app/src/main/java/br/com/luanadev/guessitapplication/screens/game/GameViewModel.kt
+++ app/src/main/java/br/com/luanadev/guessitapplication/screens/game/GameViewModel.kt
@@ -6,73 +6,71 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-
-
 /**
  * ViewModel containing all the logic needed to run the game
  */
 class GameViewModel : ViewModel() {
 
-        companion object {
-            // These represent different important times in the game, such as game length.
+    companion object {
+        // These represent different important times in the game, such as game length.
 
-            // This is when the game is over
-            private const val DONE = 0L
+        // This is when the game is over
+        private const val DONE = 0L
 
-            // This is the number of milliseconds in a second
-            private const val ONE_SECOND = 1000L
+        // This is the number of milliseconds in a second
+        private const val ONE_SECOND = 1000L
 
-            // This is the total time of the game
-            private const val COUNTDOWN_TIME = 60000L
+        // This is the total time of the game
+        private const val COUNTDOWN_TIME = 60000L
 
-        }
+    }
 
-        private val timer: CountDownTimer
+    private val timer: CountDownTimer
 
-        private val _currentTime = MutableLiveData<Long>()
-        val currentTime: LiveData<Long>
-            get() = _currentTime
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
 
-        // The current word
-        private val _word = MutableLiveData<String>()
-        val word: LiveData<String>
-            get() = _word
-
-
-        // The current score
-        private val _score = MutableLiveData<Int>()
-        val score: LiveData<Int>
-            get() = _score
+    // The current word
+    private val _word = MutableLiveData<String>()
+    val word: LiveData<String>
+        get() = _word
 
 
-        // The list of words - the front of the list is the next word to guess
-        private lateinit var wordList: MutableList<String>
+    // The current score
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
 
-        // Event which triggers the end of the game
-        private val _eventGameFinish = MutableLiveData<Boolean>()
-        val eventGameFinish: LiveData<Boolean>
-            get() = _eventGameFinish
 
-        init {
-            resetList()
-            nextWord()
-            _score.value = 0
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
 
-            // Creates a timer which triggers the end of the game when it finishes
-            timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+    // Event which triggers the end of the game
+    private val _eventGameFinish = MutableLiveData<Boolean>()
+    val eventGameFinish: LiveData<Boolean>
+        get() = _eventGameFinish
 
-                override fun onTick(millisUntilFinished: Long) {
-                    _currentTime.value = (millisUntilFinished / ONE_SECOND)
-                }
+    init {
+        resetList()
+        nextWord()
+        _score.value = 0
 
-                override fun onFinish() {
-                    _currentTime.value = DONE
-                    _eventGameFinish.value = true
-                }
+        // Creates a timer which triggers the end of the game when it finishes
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = (millisUntilFinished / ONE_SECOND)
             }
 
-            timer.start()
+            override fun onFinish() {
+                _currentTime.value = DONE
+                _eventGameFinish.value = true
+            }
         }
+
+        timer.start()
+    }
 
     /**
      * Resets the list of words and randomizes the order
@@ -97,7 +95,7 @@ class GameViewModel : ViewModel() {
             "carro",
             "Corvo",
             "comércio",
-            "saco",
+            "sapo",
             "lista",
             "bolha"
         )
